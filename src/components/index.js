f7a2c1ab7ab847f60ef6ac18473261219bc6f468
lixia <lx19890505@163.com>
2017-08-27 19:25:37 +0800

    实例化公共组件

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,7 @@ import Vue from 'vue'
 import Header from './common/header'
 import Sidebar from './common/sidebar'
 import Footer from './common/footer'
+
+Vue.component('app-header', Header)
+Vue.component('app-sidebar', Sidebar)
+Vue.component('app-footer', Footer)
